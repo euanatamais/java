0b42168347ced067ace867648a73fec16604baad
euanatamais <128156788+euanatamais@users.noreply.github.com>
2024-10-31 08:15:09 -0300

    .

---
+++ maim.java
@@ -0,0 +1,13 @@
+class helloworld {
+    public static void main(String[] args) {
+      System.out.println("Esse e o Screen Match");
+      System.out.println("Barbie");
+      
+      int ano = 2006;
+      boolean incluindoNoPlano = true;
+      double notaFilme = 5.0;
+      
+      System.out.println("Ano de lançamento " + ano);
+      
+  }
+}
